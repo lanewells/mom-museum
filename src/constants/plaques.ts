e17d9f3917fcce73b6d2b1c1273bb12980002caf
lanewells <180503664+lanewells@users.noreply.github.com>
2026-05-15 22:14:24 -0700

    parse plaques constant and type into separate folder

---
+++ src/constants/plaques.ts
@@ -0,0 +1,223 @@
+export const PLAQUES: Record<string, { title: string; description: string }> = {
+  ballet_shoes: {
+    title: "Ballet Shoes",
+    description:
+      "The shoes that inspired Mom to give Delaney a childhood spent dancing."
+  },
+  book_stack_gay: {
+    title: "LGBTQ+ Books",
+    description:
+      "Mom was instrumental in putting LGBTQ+ literature in LAUSD schools."
+  },
+  book_gay_1: {
+    title: "LGBTQ+ Books",
+    description:
+      "Mom was instrumental in putting LGBTQ+ literature in LAUSD schools."
+  },
+  book_gay_2: {
+    title: "LGBTQ+ Books",
+    description:
+      "Mom was instrumental in putting LGBTQ+ literature in LAUSD schools."
+  },
+  books_stack_travel: {
+    title: "Travel Books",
+    description: "Books to support and inform her world exploration."
+  },
+  book_2: {
+    title: "Travel Books",
+    description: "Books to support and inform her world exploration."
+  },
+  book_4: {
+    title: "Travel Books",
+    description: "Books to support and inform her world exploration."
+  },
+  book_whangdoodles: {
+    title: "The Last of the Really Great Whangdoodles",
+    description: "Mom introduced countless books to her children early on."
+  },
+  box_of_seeds: {
+    title: "Plant Seeds",
+    description:
+      "Throughout her life thus far, Mom has exercised her green thumb through gardening and sharing the practice with her children."
+  },
+  seed_packets_on_counter: {
+    title: "Plant Seeds",
+    description:
+      "Throughout her life thus far, Mom has exercised her green thumb through gardening and sharing the practice with her children."
+  },
+  candy_land1: {
+    title: "Board Games",
+    description: "She never turns down an invitation to play a good game."
+  },
+  backgammon_board_game1: {
+    title: "Board Games",
+    description: "She never turns down an invitation to play a good game."
+  },
+  Board_Game__28_1: {
+    title: "Board Games",
+    description: "She never turns down an invitation to play a good game."
+  },
+  game_one_night: {
+    title: "Board Games",
+    description: "She never turns down an invitation to play a good game."
+  },
+  cd: {
+    title: "CD",
+    description:
+      "In the early 2000s, Mom could be found singing to the Chicks in her red Toyota 4Runner and filling Delaney in on pieces of history through storytelling about the band's early political activism."
+  },
+  collage_magazine: {
+    title: "Collage Magazine",
+    description:
+      "Mom and Delaney began a tradition of collaging vision boards every new year together since their first New Year's Eve living in an apartment just the two of them."
+  },
+  boots: { title: "Cowgirl Boots", description: "Mom's current dance shoes." },
+  boot1: { title: "Cowgirl Boots", description: "Mom's current dance shoes." },
+  boot2: { title: "Cowgirl Boots", description: "Mom's current dance shoes." },
+  dog_toys: {
+    title: "Dog Bed & Toys",
+    description: "She was also Mom to Buddy, and now Twyla, Charly."
+  },
+  dog_bed: {
+    title: "Dog Bed & Toys",
+    description: "She was also Mom to Buddy, and now Twyla, Charly."
+  },
+  envelope_mothers_day: {
+    title: "Happy Mother's Day!",
+    description: "Delaney wishes Mom a very happy Mother's Day in 2026."
+  },
+  book_1: {
+    title: "Cookbooks",
+    description:
+      "Mom is a very experienced and talented cook and baker. Fan favorites include squiggle pancakes, Mac n' cheese with bacon (Delaney), and twice-baked potatoes (Gwendolyn)."
+  },
+  book_3: {
+    title: "Cookbooks",
+    description:
+      "Mom is a very experienced and talented cook and baker. Fan favorites include squiggle pancakes, Mac n' cheese with bacon (Delaney), and twice-baked potatoes (Gwendolyn)."
+  },
+  desktop_computer: {
+    title: "Diner Dash",
+    description:
+      "One of the many staples of childhood summers for Mom's children was gathering around the computer to watch Mom beat the hardest level of Diner Dash, Burger Bot, or Virtual Farm, Mom could always crack the levels Lynn and Lane were stuck on."
+  },
+  diplomas: {
+    title: "Diplomas",
+    description:
+      "Mom has 2 degrees she worked hard for while juggling raising kids and navigating her career, including a Bachelor's in Liberal Arts and Sciences, Liberal Studies and a Master's of Educational Leadership and Administration"
+  },
+  envelope_green: {
+    title: "Outgoing Mail",
+    description:
+      "Mom is the USPS's favorite patron. Her showing her affection through gifts and words of affirmation through sending mail influenced child Delaney's own love language, gift-giving, and standards."
+  },
+  envelope_pink: {
+    title: "Outgoing Mail",
+    description:
+      "Mom is the USPS's favorite patron. Her showing her affection through gifts and words of affirmation through sending mail influenced child Delaney's own love language, gift-giving, and standards."
+  },
+  Envelopes1: {
+    title: "Outgoing Mail",
+    description:
+      "Mom is the USPS's favorite patron. Her showing her affection through gifts and words of affirmation through sending mail influenced child Delaney's own love language, gift-giving, and standards."
+  },
+  mail_holder: {
+    title: "Outgoing Mail",
+    description:
+      "Mom is the USPS's favorite patron. Her showing her affection through gifts and words of affirmation through sending mail influenced child Delaney's own love language, gift-giving, and standards."
+  },
+  map_pins: {
+    title: "Map of Travels",
+    description:
+      "Her traveling has become bolder and more daring with each chapter of her life."
+  },
+  world_map: {
+    title: "Map of Travels",
+    description:
+      "Her traveling has become bolder and more daring with each chapter of her life."
+  },
+  pic_kevin: {
+    title: "Kevin (Digital Photography)",
+    description:
+      "Mom chose Kevin as a travel, nesting, and romantic partner for life."
+  },
+  pic_lane_mom: {
+    title: "Mom and Delaney (Digital Photography)",
+    description:
+      "Mom is a good and enthusiastic sport when she gets roped into things like themed photoshoots."
+  },
+  pic_lane_lynn: {
+    title: "Lane and Lynn (Photography Print)",
+    description:
+      "Her two genetic children, Lane and Lynn, cross the world to spend time together."
+  },
+  pic_mom: {
+    title: "Mom (Digital Photography)",
+    description:
+      "This piece exhibits a playful and silly side of Mom, as evidenced by her wig use and happy demeanor."
+  },
+  pic_on_desk: {
+    title: "Delaney's Senior Prom (2015)",
+    description:
+      "Taken in 2015, this version of Mom marked the first few years of her independence period, characterized by self-discovery, exploration, and writing life as her own. But she showed up in ways her children may never truly know the extent. And always for the important things: Delaney's Senior Prom, pictured."
+  },
+  keys_audi_fob: {
+    title: "Audi Keys",
+    description:
+      "For more than a decade, Mom had dreamt of driving an Audi of her own—a dream which she fulfilled in the 2020s and now drives in the country of California."
+  },
+  keys_keychain: {
+    title: "Audi Keys",
+    description:
+      "For more than a decade, Mom had dreamt of driving an Audi of her own—a dream which she fulfilled in the 2020s and now drives in the country of California."
+  },
+  keys_many: {
+    title: "Keys",
+    description:
+      "For most of her later career as a Principal in LAUSD, Mom carried an amount of keys symbolic to the responsibilities she held as a leader of educators, administrators, and young students."
+  },
+  keys_toyota: { title: "Toyota Keys", description: "4Runner, Rav4." },
+  space_heater: {
+    title: "Space Heater",
+    description: "A trademark of Mom's livingspace necessities."
+  },
+  sheet_music: {
+    title: "Sheet Music",
+    description:
+      "A pianist since childhood, Mom can still break out a tune on a piano."
+  },
+  stuffed_animal__1_1: {
+    title: "Amigurumi",
+    description:
+      "Crochet was a hobby recurrent in different stages of Mom's life, and one she passed on to child Delaney."
+  },
+  stuffed_animal_1: {
+    title: "Amigurumi",
+    description:
+      "Crochet was a hobby recurrent in different stages of Mom's life, and one she passed on to child Delaney."
+  },
+  tea: {
+    title: "Iced Tea",
+    description:
+      "Strong Lipton black iced tea with extra ice and a lemon wedge."
+  },
+  Sewing_Machine1: {
+    title: "Sewing Machine",
+    description:
+      "Mom is a talented sewer. She has made costumes, bedding, and more for her family and household."
+  },
+  scrapbook: {
+    title: "Scrapbook",
+    description:
+      "Mom has compiled multitudinous photo albums decorated with care, which her family will continue to cherish and be thankful for."
+  }
+}
+
+export function findPlaqueName(obj: any): string | null {
+  let current: any = obj
+  while (current) {
+    if (PLAQUES[current.name]) return current.name
+    current = current.parent
+  }
+  return null
+}
